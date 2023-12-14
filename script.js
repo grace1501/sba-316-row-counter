7ae497f49786439279ba3dc26303e3de2aa5167f
Grace Do <61570746+grace1501@users.noreply.github.com>
2023-12-13 21:32:12 -0600

    get all elements needed from html

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+A row counter app for yarn projects, similar to a doto list with more complexity.
+The user can add new project in a form, with the fields: project name, project type (knit or crochet), hook/needles size, yarn type/ yarn weight, notes about the project, date created?. The project name will be the key to retrieve info later (maybe display as a list on screen then user can click to access the counter).
+
+The counter start with 0, with two button add + and subtract -
+Each time the user click on the button, the appropriate event handler will fire.
+
+The user can add/edit/delete the saved projects in localStorage.
+The projects will be displayed as a table on index.html. There would be button at the end of row to access the poject (resume or edit details).
+
+*/
+
+const addProjectForm = document.querySelector("form");
+const currentProjectNameDisplay = document.getElementById('current-project-name')
